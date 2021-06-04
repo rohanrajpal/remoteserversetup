67c803839c44d8de6077237edb2ab46a835619bd
rohanrajpal <rohan46000@gmail.com>
2021-06-04 19:47:18 +0530

    add more scripts

---
+++ postgres.sh
@@ -0,0 +1,3 @@
+sudo apt install postgresql postgresql-contrib
+sudo apt install libpq-dev
+
---
+++ tinyproxy.sh
@@ -0,0 +1,3 @@
+sudo apt-get install tinyproxy
+sudo vim /etc/tinyproxy/tinyproxy.conf
+sudo service tinyproxy restart
